23159d25d99f65be49d5c987b555b85b60bd874a
JimRooney <github@jimrooney.com>
2023-09-14 10:04:15 +1200

    Test Arrow

--- navbar.js
+++ navbar.js
@@ -75,7 +75,8 @@ $().ready(() => {
       <a href="performance.html">Performance</a>
       <a href="balance.html">Balance</a>
       <a href="test2.html">Test2</a>
-      <a href="test3.html">test3</a>
+      <a href="test3.html">Test3</a>
+      <a href="TestArrow.html">Arrow</a>
       <a href="#" onClick="root.clearCache()">Reset Cache</a>
       <a href="#" onClick="root.flipDemo(this)">Flip Demo</a>
     </div>
